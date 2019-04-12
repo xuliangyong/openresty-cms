65ea00c9279133a8482bdfeb4116f2e4a0f8c3ee
xuliangyong <15619801002@163.com>
2019-04-12 09:26:47 +0800

    Update config.lua
    去掉密码

--- lua/config.lua
+++ lua/config.lua
@@ -4,15 +4,15 @@ local _M = {}
 _M["redisMaster"] = {
 	["host"] = "10.1.102.169",
 	["port"] = "6379",
-	["password"] = "foobared"
+	["password"] = ""
 }
 
 _M["redisOld"] = {
 	["host"] = "10.1.102.169",
 	["port"] = "6378",
-	["password"] = "GxyJ.,Redis6&*!"
+	["password"] = ""
 }
 
-_M["url"] = "http://www.gxyj.com"   --redis如果挂了，从这个url取
+_M["url"] = "http://www.xxx.com"   --redis如果挂了，从这个url取
 
 return _M
